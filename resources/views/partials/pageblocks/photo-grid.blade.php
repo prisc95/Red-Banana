93454d4adfac514ae290aea5325e8740be229537
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-03-05 10:50:13 +0100

    Photo-grid pageblock set-up

---
+++ resources/views/partials/pageblocks/photo-grid.blade.php
@@ -0,0 +1,13 @@
+<section class="photo-grid">
+    <div class="photo-grid-container">
+        <h2>Ontdek ons assortiment</h2>
+            <div class="photo-grid-gallery">
+                <div class="photo-grid-images row-2 col-3" id="item-1">Apple Iphone 13</div>
+                <div class="photo-grid-images row-2 col-3" id="item-2">Samsung</div>
+                <div class="photo-grid-images row-2 col-2" id="item-3">Huawei</div>
+                <div class="photo-grid-images row-2 col-2" id="item-4">Telefoonhoesjes</div>
+                <div class="photo-grid-images row-1 col-2" id="item-5">Reparaties</div>
+                <div class="photo-grid-images row-1 col-2" id="item-6">Overige accessoires</div>
+            </div>
+        </div>
+    </section>
