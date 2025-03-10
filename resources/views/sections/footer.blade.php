3f93272b2dc6b2fb75ec9a060c05b3839798b1dd
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-03-10 14:11:46 +0100

    Pageblock Added Footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,65 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
-</footer>
+
+
+{{-- <footer class="content-info"> --}}
+  {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
+
+  <section class="footer">
+  <div class="container">
+    <div class="contact">
+        <h2>Testshop</h2>
+        <p>Borchwerf 4E</p>
+        <p>4704 RG Roosendaal</p>
+        <p>0165 - 32 78 08</p>
+        <p>info@redbanana.nl</p>
+    </div>
+    <div class="openingstijden">
+        <h2>Openingtijden</h2>
+        <p>Maandag:	09:00 - 17:00</p>
+        <p>Dinsdag: 09:00 - 17:00</p>
+        <p>Woensdag: 09:00 - 17:00</p>
+        <p>Donderdag: 09:00 - 17:00</p>
+        <p>Vrijdag:	09:00 - 17:00</p>
+        <p>Zaterdag: 09:00 - 17:00</p>
+        <p>Zondag: GESLOTEN</p>
+    </div>
+    <div class="klantenservice">
+      <h2>Klantenservice</h2>
+      <div class="inner">
+    <div class="left">
+        <a href="">Over Testshop</a>
+        <a href="">Verzenden & Retourneren</a>
+        <a href="">Betaalmethoden</a>
+        <a href="">Klantenservice</a>
+    </div>
+    <div class="right">
+        <a href="">Disclaimer</a>
+        <a href="">Privacy Policy</a>
+        <a href="">Sitemap</a>
+        <a href="">Algemene Voorwaarden</a>
+    </div>
+  </div>
+    </div>
+    <div class="nieuwsbrief">
+        <h2>Nieuwsbrief</h2>
+        <div class="nieuwsbrief-inner">
+        <div class="button-container">
+        <input type="text" placeholder="Jouw emailadres">
+        <button>Inschrijven</button>
+        </div>    
+        <div class="wide-logo-container">
+            <div class="wide-logo"><img class="wide-logo-1" src="wp-content/themes/sage/resources/images/wide-logo-1.png" alt="Thuiswinkel Waarborg"></div>
+            <div class="wide-logo"><img class="wide-logo-2" src="wp-content/themes/sage/resources/images/wide-logo-2.png" alt="Keurmerk"></div>
+            </div>
+            <div class="small-logo-container">
+            <div class="small-logo small-logo-1"><img src="wp-content/themes/sage/resources/images/iDeal.png" alt="iDeal"></div>
+            <div class="small-logo small-logo-2"><img src="wp-content/themes/sage/resources/images/Visa.png" alt="Visa Mastercard"></div>
+            <div class="small-logo small-logo-3"><img src="wp-content/themes/sage/resources/images/Klarna.png" alt="Klarna"></div>
+            <div class="small-logo small-logo-4"><img src="wp-content/themes/sage/resources/images/BanContact.png" alt="BanContact"></div>
+            <div class="small-logo small-logo-5"><img src="wp-content/themes/sage/resources/images/SoFort.png" alt="SoFort"></div>
+        </div>
+    </div>
+    </div>
+</div>
+{{-- </footer> --}}
+
+</section>
