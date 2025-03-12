8220162b624d529f7793ec30ce5262ea14253ef6
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-03-12 12:57:00 +0100

    Fixed scroll content

--- resources/views/partials/pageblocks/product-swiper.blade.php
+++ resources/views/partials/pageblocks/product-swiper.blade.php
@@ -90,6 +90,40 @@
             </div>
         </div>
           </div>
+
+          <div class="swiper-slide">
+            <div class="top-container">
+                <button><i class="fa-regular fa-heart"></i></button>
+            </div>
+            <div class="center-container">
+            <div class="center-container-left"><img src="wp-content/themes/sage/resources/images/Rating.png" alt="Rating"></div>
+            <div class="center-container-right"><img src="wp-content/themes/sage/resources/images/Voorraad.png" alt="Voorraad"></div>
+            </div>
+            <div class="bottom-container">
+            <p>Leren hoesje met MagSafe voor iPhone 13 Pro Max</p>
+            <div class="price-cart-container">
+            <div class="price-container"><p id="old-price">€80,00</p><p id="new-price">€65,00</p></div>
+            <button><i class="fa-solid fa-bag-shopping"></i></button>
+            </div>
+        </div>
+          </div>
+
+          <div class="swiper-slide">
+            <div class="top-container">
+                <button><i class="fa-regular fa-heart"></i></button>
+            </div>
+            <div class="center-container">
+            <div class="center-container-left"><img src="wp-content/themes/sage/resources/images/Rating.png" alt="Rating"></div>
+            <div class="center-container-right"><img src="wp-content/themes/sage/resources/images/Voorraad.png" alt="Voorraad"></div>
+            </div>
+            <div class="bottom-container">
+            <p>Leren hoesje met MagSafe voor iPhone 13 Pro Max</p>
+            <div class="price-cart-container">
+            <div class="price-container"><p id="old-price">€80,00</p><p id="new-price">€65,00</p></div>
+            <button><i class="fa-solid fa-bag-shopping"></i></button>
+            </div>
+        </div>
+          </div>
          
         
         </div>
